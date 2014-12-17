509c2fcfe59125106738dad459328b5613f00876
Aqib Gatoo <aqib.droid@gmail.com>
2014-12-17 22:02:29 +0530

    initial commit

---
+++ app/src/main/java/com/aqibgatoo/thetest/controller/TestApplication.java
@@ -0,0 +1,18 @@
+package com.aqibgatoo.thetest.controller;
+
+import android.app.Application;
+
+/**
+ * Created by Aqib on 12/17/2014.
+ */
+public class TestApplication extends Application {
+
+    public TestApplication() {
+        super();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
